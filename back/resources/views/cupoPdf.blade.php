da373ccebd76bacf8ac52ecf950f38678a22d2b9
adimerpaul <adimer101@gmail.com>
2022-11-04 08:57:29 -0400

    Update cupoPdf.blade.php

--- back/resources/views/cupoPdf.blade.php
+++ back/resources/views/cupoPdf.blade.php
@@ -19,12 +19,12 @@
 <body>
 @foreach($cupos as $cupo)
     @if($loop->index%2==0)
-        <img src="folleto.jpg" style="  position: absolute;top: 5px;left: 5px;width: 550px;height: 550px" >
-        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="position: absolute;top: 575px;left: 420px;width: 85px;height: 85px">
+        <img src="folleto.jpg" style="  position: absolute;top: 5px;left: 5px;width: 518px;height: 800px" >
+        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="position: absolute;top: 575px;left: 420px;width: 90px;height: 90px">
         <div style="position: absolute;top: 753px;left: 490px; font-weight: bold;width: 85px;height: 85px;font-family: 'Nunito', sans-serif;color: #a0aec0">{{$cupo['id']}}</div>
     @else
-        <img src="folleto.jpg" style="  position: absolute;top: 5px;right: 5px;width: 550px;height: 550px" >
-        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="position: absolute;top: 575px;right: 25px;width: 85px;height: 85px">
+        <img src="folleto.jpg" style="  position: absolute;top: 5px;right: 5px;width: 518px;height: 800px" >
+        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="position: absolute;top: 575px;right: 25px;width: 90px;height: 90px">
         <div style="position: absolute;top: 753px;right: -50px; font-weight: bold;width: 85px;height: 85px;font-family: 'Nunito', sans-serif;color: #a0aec0">{{$cupo['id']}}</div>
         <div class="page_break"></div>
     @endif
