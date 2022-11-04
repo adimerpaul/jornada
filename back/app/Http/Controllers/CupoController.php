541c3e20d66589f5aaf6d70a63bb4d67323a3d7b
adimerpaul <adimer101@gmail.com>
2022-11-04 11:28:58 -0400

    Update CupoController.php

--- back/app/Http/Controllers/CupoController.php
+++ back/app/Http/Controllers/CupoController.php
@@ -44,7 +44,7 @@ class CupoController extends Controller
         $img = Image::make('imagenes/'.$request->foto);
 
 // rotate image 45 degrees clockwise
-        $img->rotate(90);
+        $img->rotate(90)->save('imagenes/a'.$request->foto);
 //        $file = public_path('/imagenes/'.$request->foto);
 //        $img = Image::make($file);
 //        $image = $request->file('foto');
