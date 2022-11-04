ec493a3179018803af53a613adf939f66ba06da9
adimerpaul <adimer101@gmail.com>
2022-11-04 09:59:49 -0400

    new

--- front/src/pages/Cupo.vue
+++ front/src/pages/Cupo.vue
@@ -98,7 +98,7 @@ export default {
     },
     rotateFoto(row){
       this.$q.loading.show()
-      this.$axios.post(this.url+'rotateFoto',{foto:row.foto})
+      this.$api.post(this.url+'rotateFoto',{foto:row.foto})
         .then(response => {
           this.$q.loading.hide()
           this.cupoGet()
