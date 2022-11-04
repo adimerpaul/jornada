122a89c5fc27725e4bad3a394fede3f92af7ed4d
adimerpaul <adimer101@gmail.com>
2022-11-04 14:09:29 -0400

    a

---
+++ front/src/pages/Material.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <h2>Material</h2>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: `Material`
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/pages/Refrigerio.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <h2>Refrigerio</h2>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: `Refrigerio`
+}
+</script>
+
+<style scoped>
+
+</style>
