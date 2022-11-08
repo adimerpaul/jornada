859d819a9fe2af8691b840d900b3e861e9246a92
adimerpaul <adimer101@gmail.com>
2022-11-07 22:37:51 -0400

    Update cupoPdf.blade.php

--- back/resources/views/cupoPdf.blade.php
+++ back/resources/views/cupoPdf.blade.php
@@ -20,14 +20,14 @@
 @foreach($cupos as $cupo)
     @if($loop->index%2==0)
         <img src="folleto.jpg" style="  position: absolute;top: 5px;left: 5px;width: 518px;height: 800px" >
-        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 575px;left: 420px;width: 90px;height: 90px">
+        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 525px;left: 420px;width: 90px;height: 90px">
         <div style="position: absolute;top: 753px;left: 490px; font-weight: bold;width: 85px;height: 85px;font-family: 'Nunito', sans-serif;color: #a0aec0">{{$cupo['id']}}</div>
     @else
         <img src="folleto.jpg" style="  position: absolute;top: 5px;right: 5px;width: 518px;height: 800px" >
-        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 575px;right: 25px;width: 90px;height: 90px">
+        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 525px;right: 25px;width: 90px;height: 90px">
         <div style="position: absolute;top: 753px;right: -50px; font-weight: bold;width: 85px;height: 85px;font-family: 'Nunito', sans-serif;color: #a0aec0">{{$cupo['id']}}</div>
 {{--        @if (count($cupos) == $loop->index+1)--}}
-{{--            <div class="page_break"></div>--}}
+            <div class="page_break"></div>
 {{--        @endif--}}
 
     @endif
