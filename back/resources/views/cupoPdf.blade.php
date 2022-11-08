7510b8526554bae881cda92c61f5c1f27884dfde
adimerpaul <adimer101@gmail.com>
2022-11-07 23:54:53 -0400

    Update cupoPdf.blade.php

--- back/resources/views/cupoPdf.blade.php
+++ back/resources/views/cupoPdf.blade.php
@@ -28,7 +28,7 @@
         <img src="folleto.jpg" style="  position: absolute;top: 5px;right: 5px;width: 518px;height: 800px" >
         <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 530px;right: 25px;width: 90px;height: 90px">
         <div style="position: absolute;top: 753px;right: -50px; font-weight: bold;width: 85px;height: 85px;font-family: 'Nunito', sans-serif;color: #a0aec0">{{$cupo['id']}}</div>
-        <div style="position: absolute;top: 400px;left: 580px;width: 300px;height: 15px;font-family: 'Nunito', sans-serif;color: #000;padding: 5px;background: #f6f9fc ;border: 2px solid #22254b;border-radius: 5px">Nombre..............................</div>
+        <div style="position: absolute;top: 400px;left: 550px;width: 300px;height: 15px;font-family: 'Nunito', sans-serif;color: #000;padding: 5px;background: #f6f9fc ;border: 2px solid #22254b;border-radius: 5px">Nombre..............................</div>
 {{--        @if (count($cupos) == $loop->index+1)--}}
             <div class="page_break"></div>
 {{--        @endif--}}
