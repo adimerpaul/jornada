1ce421f3b6f8ed8a91bfc58ce8e17441422f6c59
adimerpaul <adimer101@gmail.com>
2022-11-08 23:11:06 -0400

    Update credencialPdf.blade.php

--- back/resources/views/credencialPdf.blade.php
+++ back/resources/views/credencialPdf.blade.php
@@ -19,7 +19,7 @@
 <body>
 @foreach($credencials as $credencial)
 <img src="certificado.jpg" style="  position: absolute;top: 5px;left: 785px;width: 259px;height: 400px" >
-<img src="imagenes/{{$credencial['foto']}}" style="  position: absolute;top: 150px;left: 880px;width: 65px;height: 65px" >
+<img src="imagenes/{{$credencial['foto']}}" style="  position: absolute;top: 150px;left: 880px;width: 66px;height: 66px;border-radius: 33px" >
 <div style="text-align: center;position: absolute;top: 230px;left: 800px;font-size: 15px; font-weight: bold;width: 220px;height: 40px;font-family: 'Nunito', sans-serif;color: #373e48">{{$credencial['nombres']}}</div>
 <img src="data:image/png;base64, {!! $credencial['qr'] !!}" style="border:2px solid white;position: absolute;top: 275px;left: 830px;width: 170px;height: 60px">
 <div style="color: #a0aec0;position: absolute;top: 350px;left: 915px;width: 10px;height: 10px">{{$credencial['id']}}</div>
