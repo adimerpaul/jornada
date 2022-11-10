d0a88cc496388602b86d42206db9edb03c2501dc
adimerpaul <adimer101@gmail.com>
2022-11-09 23:14:22 -0400

    Update credencialPdf.blade.php

--- back/resources/views/credencialPdf.blade.php
+++ back/resources/views/credencialPdf.blade.php
@@ -13,16 +13,16 @@
             border: 0px;
         }
         .page_break { page-break-before: always; }
-
+        @page { size: 8.5cm 10.5cm; }
     </style>
 </head>
 <body>
 @foreach($credencials as $credencial)
-<img src="certificado.jpg" style="  position: absolute;top: 5px;left: 785px;width: 259px;height: 400px" >
-<img src="imagenes/{{$credencial['foto']}}" style="  position: absolute;top: 150px;left: 880px;width: 66px;height: 66px;border-radius: 33px" >
+{{--<img src="certificado.jpg" style="  position: absolute;top: 5px;left: 785px;width: 259px;height: 400px" >--}}
+<img src="imagenes/{{$credencial['foto']}}" style="  position: absolute;top: 60px;left: 120px;width: 66px;height: 66px;border-radius: 33px" >
 <div style="text-align: center;position: absolute;top: 230px;left: 800px;font-size: 15px; font-weight: bold;width: 220px;height: 40px;font-family: 'Nunito', sans-serif;color: #373e48">{{$credencial['nombres']}}</div>
-<img src="data:image/png;base64, {!! $credencial['qr'] !!}" style="border:2px solid white;position: absolute;top: 275px;left: 830px;width: 170px;height: 60px">
-<div style="color: #a0aec0;position: absolute;top: 350px;left: 915px;width: 10px;height: 10px">{{$credencial['id']}}</div>
+<img src="data:image/png;base64, {!! $credencial['qr'] !!}" style="border:2px solid white;position: absolute;top: 150px;left: 60px;width: 170px;height: 60px">
+{{--<div style="color: #a0aec0;position: absolute;top: 350px;left: 915px;width: 10px;height: 10px">{{$credencial['id']}}</div>--}}
 <div class="page_break"></div>
 @endforeach
 </body>
