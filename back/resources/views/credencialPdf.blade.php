f2f84147e3c320b91cccb3c4e708b57413da64cd
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2022-11-10 23:50:42 -0400

    nom

--- back/resources/views/credencialPdf.blade.php
+++ back/resources/views/credencialPdf.blade.php
@@ -20,7 +20,7 @@
 @foreach($credencials as $credencial)
 {{--<img src="certificado.jpg" style="  position: absolute;top: 5px;left: 785px;width: 259px;height: 400px" >--}}
 <img src="imagenes/{{$credencial['foto']}}" style="  position: absolute;top: 60px;left: 120px;width: 66px;height: 66px;border-radius: 33px" >
-<div style="text-align: center;position: absolute;top: 230px;left: 800px;font-size: 15px; font-weight: bold;width: 220px;height: 40px;font-family: 'Nunito', sans-serif;color: #373e48">{{$credencial['nombres']}}</div>
+<div style="text-align: center;position: absolute;top: 230px;left: 10px;font-size: 14px; font-weight: bold;width: 220px;height: 40px;font-family: 'Nunito', sans-serif;color: #373e48">{{$credencial['nombres']}}</div>
 <img src="data:image/png;base64, {!! $credencial['qr'] !!}" style="border:2px solid white;position: absolute;top: 150px;left: 60px;width: 170px;height: 60px">
 {{--<div style="color: #a0aec0;position: absolute;top: 350px;left: 915px;width: 10px;height: 10px">{{$credencial['id']}}</div>--}}
 <div class="page_break"></div>
