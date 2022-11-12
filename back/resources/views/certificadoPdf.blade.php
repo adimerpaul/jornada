a27518f563bd3394fbba25f436b81b3886414490
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2022-11-12 16:11:51 -0400

    mod

--- back/resources/views/certificadoPdf.blade.php
+++ back/resources/views/certificadoPdf.blade.php
@@ -20,13 +20,13 @@
 @foreach($certificados as $certificado)
     <img src="certificado.jpg" style=" position: absolute;top: 5px;left: 5px;width: 1036px;height: 800px" >
     <div style="border: 1px solid red;position: absolute;top: 310px;left: 100px;text-align: center;font-size: 2.5em; font-weight: bold;width: 850px;height: 35px;font-family: 'Nunito', sans-serif;color: #343c47">{{$certificado['nombres']}}</div>
-    <div style="border: 1px solid red;position: absolute;top: 400px;left: 100px;text-align: justify;font-size: 1em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
-        Por su participación en calidad de <span style="font-size: 1.5em;font-weight: bold;">{{$certificado['tipo']}}</span> de las II JORNADAS DE TECNOLOGIAS DE COMUNICACIÓN 2022” desarrollado del 16 al 18 de noviembre de 2022, con una carga horaria académica de 32 horas lectivas.
+    <div style="border: 1px solid red;position: absolute;top: 380px;left: 100px;text-align: justify;font-size: 1em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
+        Por su participación en calidad de <span style="font-size: 1.5em;font-weight: bold;">{{$certificado['tipo']}}</span> de las <span style="font-size: 1.5em;font-weight: bold;">II JORNADAS DE TECNOLOGIAS DE COMUNICACIÓN 2022</span> desarrollado del 16 al 18 de noviembre de 2022, con una carga horaria académica de 32 horas lectivas.
     </div>
     <div style="border: 1px solid red;position: absolute;top: 500px;left: 100px;text-align: left;font-size: 1em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
         Oruro, Noviembre de 2022
     </div>
-    <div style="border: 1px solid red;position: absolute;top: 550px;left: 100px;text-align: center;font-size: 0.8em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
+    <div style="border: 1px solid red;position: absolute;top: 550px;left: 100px;text-align: center;font-size: 0.6em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
         <table style="width: 100%">
             <tr>
                 <td style="text-align: center">MSc. Ing. Gregorio Fernando Ureña Merida<br>COORDINADOR ACADEMIA REGIONAL<br>CISCO SIS-FNI-UTO</td>
@@ -35,7 +35,7 @@
             </tr>
         </table>
     </div>
-    <img src="data:image/png;base64, {!! $certificado['qr'] !!}" style="border:2px solid white;position: absolute;top: 575px;right: 150px;width: 90px;height: 90px">
+    <img src="data:image/png;base64, {!! $certificado['qr'] !!}" style="border:2px solid white;position: absolute;top: 600px;right: 150px;width: 90px;height: 90px">
     <div class="page_break"></div>
 {{--    @if($loop->index%2==0)--}}
 {{--        <img src="folleto.jpg" style="  position: absolute;top: 5px;left: 5px;width: 518px;height: 800px" >--}}
