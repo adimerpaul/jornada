8c18f25bbe1d788851145d2fb68adc996cdf2b1f
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2022-11-13 23:02:03 -0400

    print

--- back/resources/views/certificadoPdf.blade.php
+++ back/resources/views/certificadoPdf.blade.php
@@ -40,7 +40,7 @@
         </table>
     </div>
     <img src="data:image/png;base64, {!! $certificado['qr'] !!}" style="border:2px solid white;position: absolute;top: 600px;right: 450px;width: 90px;height: 90px">
-    <div style="border: 0px solid red;position: absolute;top: 700px;left: 120px;text-align: center;font-size: 0.5em; font-weight: bold;width: 850px;height: 35px;font-family: 'Nunito', sans-serif;color: #343c47">A: {{$certificado['url']}}</div>
+    <div style="border: 0px solid red;position: absolute;top: 700px;left: 120px;text-align: center;font-size: 0.5em; font-weight: bold;width: 850px;height: 35px;font-family: 'Nunito', sans-serif;color: #343c47">{{$certificado['url']}}</div>
 
     <div class="page_break"></div>
 {{--    @if($loop->index%2==0)--}}
