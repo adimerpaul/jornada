bd98014d4e883f0f10f7dadcf8776a992daeb3a7
AFLopezG <lopez.gutierrez.a07@gmail.com>
2023-07-26 11:06:48 -0400

    MOD NAM

--- back/resources/views/cupoPdf.blade.php
+++ back/resources/views/cupoPdf.blade.php
@@ -23,12 +23,12 @@
         <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 680px;left: 395px;width: 90px;height: 90px">
         <div style="position: absolute;top: 753px;left: 490px; font-weight: bold;width: 85px;height: 85px;font-family: 'Nunito', sans-serif;color: #a0aec0">{{$cupo['id']}}</div>
 {{--        <img src="nom.jpg" style="  position: absolute;top: 300px;left: 35px;width: 150px;height: 45px" >--}}
-        <div style="position: absolute;top: 535px;left: 35px;width: 300px;height: 15px;font-family: 'Nunito', sans-serif;color: #000;padding: 5px;background: #f6f9fc ;border: 2px solid #22254b;border-radius: 5px">Nombre..............................</div>
+        <div style="position: absolute;top: 540px;left: 35px;width: 300px;height: 15px;font-family: 'Nunito', sans-serif;color: #000;padding: 5px;background: #f6f9fc ;border: 2px solid #22254b;border-radius: 5px">Nombre..............................</div>
     @else
         <img src="folleto.jpeg" style="  position: absolute;top: 5px;right: 5px;width: 518px;height: 800px" >
         <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 680px;right: 35px;width: 90px;height: 90px">
         <div style="position: absolute;top: 753px;right: -50px; font-weight: bold;width: 85px;height: 85px;font-family: 'Nunito', sans-serif;color: #a0aec0">{{$cupo['id']}}</div>
-        <div style="position: absolute;top: 535px;left: 550px;width: 300px;height: 15px;font-family: 'Nunito', sans-serif;color: #000;padding: 5px;background: #f6f9fc ;border: 2px solid #22254b;border-radius: 5px">Nombre..............................</div>
+        <div style="position: absolute;top: 540px;left: 550px;width: 300px;height: 15px;font-family: 'Nunito', sans-serif;color: #000;padding: 5px;background: #f6f9fc ;border: 2px solid #22254b;border-radius: 5px">Nombre..............................</div>
 {{--        @if (count($cupos) == $loop->index+1)--}}
             <div class="page_break"></div>
 {{--        @endif--}}
