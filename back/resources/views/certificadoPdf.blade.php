0bc4bd41f86b859fcf4d212460b7dc22030969c8
adimerpaul <adimer101@gmail.com>
2023-08-01 05:58:06 -0400

    Update certificadoPdf.blade.php

--- back/resources/views/certificadoPdf.blade.php
+++ back/resources/views/certificadoPdf.blade.php
@@ -27,9 +27,9 @@
     <div style="border: 0px solid red;position: absolute;top: 380px;left: 120px;text-align: justify;font-size: 1em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
         Por su participación en calidad de <span style="font-size: 1.5em;font-weight: bold;">{{$certificado['tipo']}}</span> del Evento <span style="font-size: 1.5em;font-weight: bold;">“PyhtonDay 2023”</span> desarrollado el 2 de agosto de 2023 con las siguientes temáticas:
         <br>
-        <div style="padding: 20px">
+        <div style="padding: 20px;padding-left: 250px">
             <ul>
-                <li>Python Aplicado a las Ciencia de la Ingeniería</li>
+                <li>Python Aplicado a la Ciencia de la Ingenieria</li>
                 <li>Python Aplicado al Internet de las Cosas</li>
             </ul>
         </div>
@@ -37,11 +37,14 @@
     <div style="border: 0px solid red;position: absolute;top: 510px;left: 150px;text-align: left;font-size: 1em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
         con una carga horaria académica de 10 horas lectivas.
     </div>
-    <div style="border: 0px solid red;position: absolute;top: 550px;left: 120px;text-align: center;font-size: 0.6em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
-        <img src="Signature.png" alt="" style="width: 100px;height: 100px;position: absolute;top: -35px;left: 70px">
-        <img src="Signature.png" alt="" style="width: 100px;height: 100px;position: absolute;top: -35px;left: 310px">
-        <img src="Signature.png" alt="" style="width: 100px;height: 100px;position: absolute;top: -35px;left: 620px">
-        <table style="width: 100%;padding: 10px">
+    <div style="border: 0px solid red;position: absolute;top: 520px;right: 120px;text-align: right;font-size: 1em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
+        Oruro, Agosto de 2023
+    </div>
+    <div style="border: 0px solid red;position: absolute;top: 575px;left: 120px;text-align: center;font-size: 0.6em;width: 850px;height: 85px;font-family: 'Nunito', sans-serif;color: #343c47">
+        <img src="Signature.png" alt="" style="width: 100px;height: 100px;position: absolute;top: -35px;z-index:-1;left: 160px">
+        <img src="Signature.png" alt="" style="width: 100px;height: 100px;position: absolute;top: -35px;z-index:-1;left: 410px">
+        <img src="Signature.png" alt="" style="width: 100px;height: 100px;position: absolute;top: -35px;z-index:-1;left: 720px">
+        <table style="width: 100%;padding: 10px;margin-left: 90px">
             <tr>
                 <td style="text-align: center">Ing. Edmar Perez Villarroel<br><b>INSTRUCTOR</b><br></td>
                 <td style="text-align: center">MSc. Ing. G. Fernando Ureña Merida<br><b>INSTRUCTOR</b><br></td>
@@ -49,10 +52,11 @@
             </tr>
         </table>
     </div>
-    <img src="data:image/png;base64, {!! $certificado['qr'] !!}" style="border:2px solid white;position: absolute;top: 600px;right: 450px;width: 90px;height: 90px">
-    <div style="border: 0px solid red;position: absolute;top: 700px;left: 120px;text-align: center;font-size: 0.5em; font-weight: bold;width: 850px;height: 35px;font-family: 'Nunito', sans-serif;color: #343c47">{{$certificado['url']}}</div>
-
-    <div class="page_break"></div>
+    <img src="data:image/png;base64, {!! $certificado['qr'] !!}" style="border:2px solid white;position: absolute;top: 630px;right: 450px;width: 90px;height: 90px">
+    <div style="border: 0px solid red;position: absolute;top: 725px;left: 120px;text-align: center;font-size: 0.5em; font-weight: bold;width: 850px;height: 35px;font-family: 'Nunito', sans-serif;color: #343c47">{{$certificado['url']}}</div>
+    @if( $loop->index !== sizeof($certificados)-1)
+        <div class="page_break"></div>
+    @endif
 {{--    @if($loop->index%2==0)--}}
 {{--        <img src="folleto.jpg" style="  position: absolute;top: 5px;left: 5px;width: 518px;height: 800px" >--}}
 {{--        <img src="data:image/png;base64, {!! $cupo['qr'] !!}" style="border:2px solid white;position: absolute;top: 575px;left: 420px;width: 90px;height: 90px">--}}
