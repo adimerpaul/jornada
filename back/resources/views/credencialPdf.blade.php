fa4fad123e3d8d83a96dfbb1ff5a942bc2b48d77
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2025-10-19 23:53:31 -0400

    mod cred

--- back/resources/views/credencialPdf.blade.php
+++ back/resources/views/credencialPdf.blade.php
@@ -23,9 +23,9 @@
 
 @endif
 <img src="imagenes/{{$credencial['foto']}}" style="position: absolute;top: 110px;left: 50px;width: 80px;height: 80px;border-radius: 40px" >
-<div style="text-align: center;position: absolute;top: 200px;left: 0px;font-size: 8px; font-weight: bold;width: 200px;height: 15px;font-family: 'Nunito', sans-serif;color: #373e48;background-color: white">{{$credencial['nombres']}}</div>
-<div style="text-align: center;position: absolute;top: 220px;left: 0px;font-size: 12px; font-weight: bold;width: 200px;height: 15px;font-family: 'Nunito', sans-serif;color: #373e48;background-color: white">{{$credencial['tipo']}}</div>
-<div style="text-align: center;position: absolute;top: 242px;left: 0px;font-size: 8px; font-weight: bold;width: 200px;height: 15px;font-family: 'Nunito', sans-serif;color: #373e48;background-color: white">{{$credencial['carrera']}}</div>
+<div style="text-align: center;position: absolute;top: 200px;left: 20px;font-size: 8px; font-weight: bold;width: 200px;height: 15px;width: 150px;height: 15px;font-family: 'Nunito', sans-serif;color: #373e48;background-color: white">{{$credencial['nombres']}}</div>
+<div style="text-align: center;position: absolute;top: 220px;left: 20px;font-size: 12px; font-weight: bold;width: 200px;height: 15px;width: 150px;font-family: 'Nunito', sans-serif;color: #373e48;background-color: white">{{$credencial['tipo']}}</div>
+<div style="text-align: center;position: absolute;top: 242px;left: 20px;font-size: 8px; font-weight: bold;width: 200px;height: 15px;width: 150px;font-family: 'Nunito', sans-serif;color: #373e48;background-color: white">{{$credencial['carrera']}}</div>
 {{--<div style="text-align: center;position: absolute;top: 320px;left: 58px;font-size: 15px; font-weight: bold;width: 200px;height: 40px;font-family: 'Nunito', sans-serif;color: red;text-align: center">{{$credencial['ci']}}</div>--}}
 <img src="data:image/png;base64, {!! $credencial['qr'] !!}" style="background : white ; border:2px solid white;position: absolute;top: 260px;left: 8px;width: 170px;height: 35px">
 {{--<div style="color: #a0aec0;position: absolute;top: 350px;left: 915px;width: 10px;height: 10px">{{$credencial['id']}}</div>--}}
