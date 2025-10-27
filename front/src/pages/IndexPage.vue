6043228ade56ad02de210fc175e4ca4b1de62094
Alejandro F. Lopez Gutierrez <lopez.gutierrez.a07@gmail.com>
2025-10-27 13:02:32 -0400

    subir

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -21,6 +21,17 @@
           </q-card-section>
         </q-card>
       </div>
+      <div class="col-12 col-sm-6 q-pa-md" v-for="r in informacion" :key="r">
+        <q-card class="bg-purple">
+          <q-card-section>
+            <div class="text-h6 text-white">{{r.nombre}} <q-btn flat dense @click="getCupoPaquete" icon="refresh" /> </div>
+          </q-card-section>
+          <q-card-section>
+            <div class="text-h2 text-white">{{r.total}}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+
     </div>
   </q-page>
 </template>
@@ -31,12 +42,20 @@ export default {
     return {
       libres: 0,
       ocupados: 0,
+      informacion: []
     }
   },
   created() {
     this.actualizar();
+    this.getCupoPaquete();
   },
   methods: {
+    getCupoPaquete() {
+        this.$api.get('totalPaquete').then((response) => {
+          console.log(response.data)
+            this.informacion = response.data
+        })
+    },
     actualizar() {
       this.$q.loading.show()
       this.$api.get('cupo')
